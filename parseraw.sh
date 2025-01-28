c564c286f9fdb6c7275184386fd1ffeec90255b4
Asashio <asashio@tsunkit.net>
2025-01-28 13:41:01 -0600

    script update

--- parseraw.sh
+++ parseraw.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+    ifile="raw.json"
+else
+    ifile="$1"
+fi
+
+if [ -z "$2" ]; then
+    ldate="date -I"
+else
+    ldate=`$2`
+fi
+
 cd ..
 cd apistart2
-if [ -f raw.json ]; then
+if [ -f "$ifile" ]; then
     echo "Beautify..."
     rm parsed/*
     mkdir parsed
-	node ../JSFormatter/handleStart2.js raw.json start2.json
+	`node ../JSFormatter/handleStart2.js $ifile start2.json`
 	if [ -f start2.json ]; then
-        rm raw.json
-		git add -u :/
+        rm $ifile
+        git add -u :/
         git add *.json
-		ldate=`date -I`
 		git commit -m "Update $ldate"
         git push
 	fi
+else
+    echo "Nothing to do.."
 fi
